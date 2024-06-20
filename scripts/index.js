825e2c114f31f8a0394a7d55de2538d96fdcce09
Ridy Rousseau <aixciv@yahoo.com>
2024-06-20 10:08:02 -0400

    Project 4 First Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
-const initialElements = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
+
   {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
