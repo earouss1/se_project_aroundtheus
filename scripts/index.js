b3200f52f2dc5781ce88b3689c957ad8f23b68d6
Ridy Rousseau <aixciv@yahoo.com>
2024-06-26 15:32:21 -0400

    Project 4 Second Commit

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileExitButton = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileExitButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
