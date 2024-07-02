3c6ca6db65b202ada346c89264945a62d63ab544
Ridy Rousseau <aixciv@yahoo.com>
2024-07-02 19:10:26 -0400

    Project 4 Final Commit

--- scripts/index.js
+++ scripts/index.js
@@ -30,14 +30,79 @@ const initialCards = [
   },
 ];
 
+// ELements
+
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileExitButton = document.querySelector("#modal-close-button");
+const profileText = document.querySelector("#profile-text");
+const profileSubText = document.querySelector("#profile-sub-text");
+const profileInputText = document.querySelector("#form-profile-text");
+const profileInputSubText = document.querySelector("#form-profile-sub-text");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector(".elements__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
-});
+// Functions
 
-profileExitButton.addEventListener("click", () => {
+function closePoppup() {
   profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditform() {
+  profileInputText.value = profileText.textContent;
+  profileInputSubText.value = profileSubText.textContent;
+  profileEditModal.classList.add("modal_opened");
+}
+
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileText.textContent = profileInputText.value;
+  profileSubText.textContent = profileInputSubText.value;
+  closePoppup();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".element__image");
+  const cardTitleElement = cardElement.querySelector(
+    ".element__composite-title"
+  );
+  cardTitleElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardTitleElement.alt = data.name;
+  return cardElement;
+}
+
+// Event Listener
+
+profileEditButton.addEventListener("click", handleProfileEditform);
+
+profileExitButton.addEventListener("click", closePoppup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+// Loops
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardListElement.append(cardElement);
 });
+
+/*for (let i = 0; i < initialCards.length; i++) {
+  const data = initialCards[i];
+
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImageElement = cardElement.querySelector(".element__image");
+  const cardTitleElement = cardElement.querySelector(
+    ".element__composite-title"
+  );
+
+  cardListElement.prepend(cardElement);
+
+  cardTitleElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardTitleElement.alt = data.name;
+}*/
